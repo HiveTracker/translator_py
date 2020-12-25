c210297cc6d64c6a2b56c3259965bb354d1be48f
Cedric Honnet <honnet@telecom-paristech.org>
2020-12-24 22:57:21 -0800

    Basic data parsing OK - TODO: handle checksum.

--- translator.py
+++ translator.py
@@ -1,71 +1,69 @@
 #!/usr/bin/python
+import sys
 
+DEBUG_PRINT = 0
+
+###############################################################################
 class Reception:
     f = open("test1.bin", "rb")
+    base_axis = 0
 
     def __init__(self):
         print("init")
-        self.f
 
+    ###########################################################################
     def lookForHeader(self):
+        if DEBUG_PRINT: print("\n Getting header:")
         # packets structure:
-        # 2 headers + 1 base_axis + 4 photodiodes*2 bytes
-        while True:
-            while int("{0:b}".format(ord(self.readByte())),2) != 255:
-                pass # consume
+        # 2 headers (with base_axis and checksum) + 4 photodiodes * 2 bytes
 
-            if int("{0:b}".format(ord(self.readByte())),2) != 255:
-                continue
+        if (self.readByte(True) & 0x90) != 0x80:
+            print("HEADER 1 ERROR")
 
-            break
+        if (self.readByte(    ) & 0xF0) != 0x80:
+            print("HEADER 2 ERROR")
 
-    def readByte(self):
-        byte = self.f.read(1)   # number of bytes read
-        #print("###########\n")
-        #print(byte)
+        #TODO: handle checksum
+
+    ###########################################################################
+    def readByte(self, save_base_axis = False):
+        byte = self.f.read(1)
+        byte = int("{0:b}".format(ord(byte)),2)  # convert from string
+        if DEBUG_PRINT: print(format(byte, '08b'))
+
+        if save_base_axis:
+            self.base_axis = byte
+
         return byte
 
+    ###########################################################################
     def parse_data(self):
+        self.lookForHeader()
+
+        base = (self.base_axis >> 6) & 1
+        axis = (self.base_axis >> 5) & 1
+        if DEBUG_PRINT: print("base, axis:", base, axis,
+                              "\n\n Getting Centroids:")
         centroidNum = 4
-
-        base_axis = self.readByte()
-        base_axis = int("{0:b}".format(ord(base_axis)),2) # convert hex to int
-        
-        print(base_axis)
-        
-        base = (base_axis >> 1) & 1
-        axis = (base_axis >> 0) & 1
-
-        print("\nbase, axis:", base, axis)
-
         centroids = [0 for i in range(centroidNum)]
-
         for i in range(centroidNum):
             centroids[i] = self.decodeTime()
-            print("centroids[", i, "] =", centroids[i])
-
-        # consumes header
-        for i in range(2):
-            b = int("{0:b}".format(ord(self.readByte())),2)
-            if (b != 255):
-                print("header problem", i)
-                self.lookForHeader()
-                break
+            if DEBUG_PRINT: print("=> centroids[", i, "] =", centroids[i])
 
         return base, axis, centroids
 
+
+    ###############################################################################
     def decodeTime(self):
-        rxl = self.readByte()        # LSB first
-        rxl = int("{0:b}".format(ord(rxl)),2)   # convert hex to int
-        rxh = self.readByte()        # MSB last
-        rxh = int("{0:b}".format(ord(rxh)),2)   # convert hex to int
+        rxh = self.readByte()        # MSB first
+        rxl = self.readByte()        # LSB last
         time = (rxh << 8) + rxl     # reconstruct packets
         time <<= 2                  # (non-significant) lossy decompression
         time /= 16.                 # convert to us
 
         if (time >= 6777 or time < 1222):
+            print(" >>> INVALID TIME:", str(time))
             time = 0
-            print("INVALID TIME")
 
         return time
 
@@ -74,17 +72,23 @@ class Reception:
 # This is just for debug, or to use as example
 if __name__ == "__main__":
 
-    # create an object (it also does the port initialization)
+    # create an object (it also does the initialization)
     rx = Reception()
 
-    #base_axis = rx.readByte()
-    #print("\nbase, axis:", base_axis)
     while True:
         # base = 0 or 1 (B or C)
         # axis = 0 or 1 (horizontal or vertical)
         # centroids = array of 4 floats in microseconds
-        # accelerations = array of 3 floats in G (AKA m/s^2)
-        base, axis, centroids = rx.parse_data()
+
+        try:
+            base, axis, centroids = rx.parse_data()
+
+            if not DEBUG_PRINT:
+                print( base, axis, centroids )
+
+        except:
+            print("END OF FILE")
+            sys.exit()
 
 
 """
